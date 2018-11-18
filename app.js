9cc10d04732a10796bda935a6d6b61967c79c64d
Anthony Cheng <anthonycheng12@gmail.com>
2018-11-18 17:50:24 -0500

    part4

--- app.js
+++ app.js
@@ -1,9 +1,89 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 require('./db');
 const session = require('express-session');
 const path = require('path');
 const auth = require('./auth.js');
 
-app.listen(3000);
+const Player = mongoose.model('Player');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: false }));
+app.use(session({
+    secret: 'add session secret here!',
+    resave: false,
+    saveUninitialized: true,
+}));
+
+app.get('/', (req, res) => {
+    if(req.session.user){
+		Player.find((err, art) => {
+			if(art.length){
+				res.render('index', {user: req.session.user, players: art});
+			} else{
+				res.render('index', {user: req.session.user});
+			}
+		});
+	} else{
+		res.render('index');
+	}
+})
+
+app.get('/player/add', (req, res) => {
+    if(req.session.user){
+        res.render('player-add');
+    } else {
+        res.redirect('/login');
+    }
+})
+
+app.post('/player/add', (req, res) => {
+    if(req.session.user){
+        new Player({
+            firstname: req.body.firstname,
+            lastname: req.body.lastname
+        }).save((err) => {
+            if(err){
+                console.log("Error adding your player")
+                res.render('player-add');
+            } else {
+                res.redirect('/');
+            }
+        });
+    } else {
+        res.redirect('/login');
+    }
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+app.post('/register', (req, res) => {
+    auth.register(req.body.username, req.body.email, req.body.password, (err) => {
+        res.render('register', {message: err.message});
+    }, (user) => {auth.startAuthenticatedSession(req, user, (err) => {
+            res.redirect('/');
+        });
+    });
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.post('/login', (req, res) => {
+    auth.login(req.body.username, req.body.password, (err) => {
+        res.render('login', {message: err.message});
+    }, (user) => {auth.startAuthenticatedSession(req, user, (err) => {
+            res.redirect('/');
+        });
+    });
+});
+
+app.listen(process.env.PORT || 3000);
